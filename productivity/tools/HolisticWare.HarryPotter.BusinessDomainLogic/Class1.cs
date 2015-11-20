f90865db818928d8ede611fe90a7908e0073c705
moljac <mcvjetko@holisticware.net>
2015-11-20 19:54:14 +0100

    library HP

---
+++ productivity/tools/HolisticWare.HarryPotter.BusinessDomainLogic/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XolisticWare.HarryPotter.BusinessDomainLogic
+{
+    public class Class1
+    {
+    }
+}
